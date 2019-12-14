6a1c7430d5118dd1a5445885763f5eff2b3eff4a
hyun <rlaalgus132@naver.com>
2019-12-14 14:18:35 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191214_02_baseballgame/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a20191214_02_baseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/example/a20191214_02_baseballgame/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.a20191214_02_baseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
